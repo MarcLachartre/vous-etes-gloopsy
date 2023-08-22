d6217ac4892c6478f924e79341dc1a7145f0c116
marclachartre <marc.lachartre@gmail.com>
2023-08-22 16:37:36 +0200

    push to test facebook auth

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -13,13 +13,13 @@ export const options: NextAuthOptions = {
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
         }),
     ],          
-    callbacks: {
-        async redirect({ url, baseUrl }) {
-          // Allows relative callback URLs
-          if (url.startsWith("/")) return `${baseUrl}${url}`
-          // Allows callback URLs on the same origin
-          else if (new URL(url).origin === baseUrl) return url
-          return baseUrl
-        }
-    } 
+    // callbacks: {
+    //     async redirect({ url, baseUrl }) {
+    //       // Allows relative callback URLs
+    //       if (url.startsWith("/")) return `${baseUrl}${url}`
+    //       // Allows callback URLs on the same origin
+    //       else if (new URL(url).origin === baseUrl) return url
+    //       return baseUrl
+    //     }
+    // } 
 }
