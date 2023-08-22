49661bb035fc8d6ae47d4f572ec775b7fc0a66e4
marclachartre <marc.lachartre@gmail.com>
2023-08-22 13:19:43 +0200

    push to test facebook auth

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,17 +1,18 @@
-import type { NextAuthOptions } from 'next-auth' 
-import GitHubProvider from 'next-auth/providers/github'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import FacebookProvider from "next-auth/providers/facebook";
+// import type { NextAuthOptions } from 'next-auth' 
+// import GitHubProvider from 'next-auth/providers/github'
+// import CredentialsProvider from 'next-auth/providers/credentials'
+// import FacebookProvider from "next-auth/providers/facebook";
 
-export const options: NextAuthOptions = {
-    providers: [ 
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string
-        }),
-        FacebookProvider({
-            clientId: process.env.FACEBOOK_CLIENT_ID as string,
-            clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string
-        }),
-    ]
-}
+// export const options: NextAuthOptions = {
+//     providers: [ 
+//         GitHubProvider({
+//             clientId: process.env.GITHUB_ID as string,
+//             clientSecret: process.env.GITHUB_SECRET as string
+//         }),
+//         FacebookProvider({
+//             clientId: process.env.FACEBOOK_CLIENT_ID as string,
+//             clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string
+//         }),
+//     ]
+// }
+export{}
--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,3 @@
-export { default } from "next-auth/middleware" 
-// this line applies nextauth to our entire project
-export const config = { matcher: []} // add routes to matcher to block if not loggedin
+// export { default } from "next-auth/middleware" 
+// // this line applies nextauth to our entire project
+// export const config = { matcher: []} // add routes to matcher to block if not loggedin
