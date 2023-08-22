4d60204f040f7e33c55867812f1867cd2c914abf
marclachartre <marc.lachartre@gmail.com>
2023-08-22 13:24:49 +0200

    push to test facebook auth

--- middlewar.ts
+++ middlewar.ts
@@ -1,4 +1,4 @@
 // export { default } from "next-auth/middleware" 
 // // this line applies nextauth to our entire project
 // export const config = { matcher: []} // add routes to matcher to block if not loggedin
-export{}
+export default {}
